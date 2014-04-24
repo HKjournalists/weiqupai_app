9b5ec56888788c7b6fc40fb96491dac432affc5f
icesyc <icesyc@qq.com>
2014-04-24 23:25:23 +0800

    安卓百度推送完成

--- WeiQuPai/cordova/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/apple/weiqupai_app/WeiQuPai/cordova/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/apple/weiqupai_app/WeiQuPai/cordova/platforms/android/AndroidManifest.xml \
--- WeiQuPai/cordova/platforms/android/ant-build/android-release-unsigned.apk.d
+++
@@ -1,6 +0,0 @@
-/Users/apple/weiqupai_app/WeiQuPai/cordova/platforms/android/ant-build/android-release-unsigned.apk : \
-/Users/apple/weiqupai_app/WeiQuPai/cordova/platforms/android/ant-build/android.ap_ \
-/Users/apple/weiqupai_app/WeiQuPai/cordova/platforms/android/ant-build/classes.dex \
-/Users/apple/weiqupai_app/WeiQuPai/cordova/platforms/android/CordovaLib/ant-build/classes.jar \
-/Users/apple/weiqupai_app/WeiQuPai/cordova/platforms/android/libs/pushservice-2.4.0.jar \
-/Users/apple/weiqupai_app/WeiQuPai/cordova/platforms/android/libs/libammsdk.jar \
