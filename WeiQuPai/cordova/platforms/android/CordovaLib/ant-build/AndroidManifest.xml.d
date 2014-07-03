497ad5bbf54c34eb3ae9d51675a550b5998faaa6
icesyc <icesyc@gmail.com>
2014-07-03 18:03:51 +0800

    test

--- WeiQuPai/cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ WeiQuPai/cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/apple/weiqupai_app/WeiQuPai/cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/apple/weiqupai_app/WeiQuPai/cordova/platforms/android/CordovaLib/AndroidManifest.xml \
+/Users/cuili/weiqupai_app/WeiQuPai/cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/cuili/weiqupai_app/WeiQuPai/cordova/platforms/android/CordovaLib/AndroidManifest.xml \
--- WeiQuPai/cordova/platforms/android/CordovaLib/ant-gen/R.java.d
+++ WeiQuPai/cordova/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -1,1 +1,1 @@
- : /Users/apple/weiqupai_app/WeiQuPai/cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
+ : /Users/cuili/weiqupai_app/WeiQuPai/cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
